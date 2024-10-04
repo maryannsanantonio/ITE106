b31862ae90a71661f75a4fdeb6adb1a41808d396
maryannsanantonio <maryannsanantonio66@gmail.com>
2024-10-04 12:52:02 +0800

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers
+{
+	public static void main(String[]args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.print(" ");
+	int num1 = scanner.nextInt();
+	System.out.print(" ");
+	int num2 = scanner.nextInt();
+	int sum = num1 + num2;
+	System.out.println (sum);
+	}
+}
