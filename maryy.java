ebe350e4fed528c0ca1d04b3ccca2670f61e770b
maryannsanantonio <maryannsanantonio66@gmail.com>
2024-10-04 12:58:34 +0800

    Add files via upload

---
+++ maryy.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class maryy
+{
+	public static void main(String[]args) {
+	for (int i = 1; i <= 5000; ++i) {
+		System.out.println("SAN ANTONIO MARY ANN P.");
+		}
+	}
+}
