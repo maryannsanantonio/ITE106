5382535704e18263747cdff09f8995672e0eb6d5
maryannsanantonio <maryannsanantonio66@gmail.com>
2024-10-04 13:57:39 +0800

    Add files via upload

---
+++ BicycleMain.java
@@ -0,0 +1,9 @@
+public class BicycleMain {
+	public static void main (String[] args) {
+		
+		Bicycle sportsBicycle = new Bicycle();
+		
+		System.out.println(sportsBicycle.gear);
+		sportsBicycle.braking();
+	}
+}
